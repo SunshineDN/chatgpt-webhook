31c779f9b5cd89900f4bd05ac40133e86043414a
Douglas Augusto <sunshinedn2003@gmail.com>
2024-06-01 08:10:56 -0300

    removed fs and path

--- src/controllers/gptController.js
+++ src/controllers/gptController.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-const path = require('path');
-const fs = require('fs');
 const axios = require('axios');
 const OpenAI = require('openai');
 const { downloadAudio, deleteTempFile } = require('../services/DaD-Audio');
